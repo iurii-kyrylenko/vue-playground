2b1823254d94a23cd0fba0fa544a3b25d11f9a32
iurii-kyrylenko <yuriproject@gmail.com>
2017-03-06 15:57:41 +0200

    Fixed styles for navbar

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,13 @@
       <img src="./assets/logo.png">
       <div class="container">
         <nav class="navbar navbar-default">
+        <div class="container-fluid">
           <ul class="nav navbar-nav">
-            <li><router-link to="/">Home</router-link></li>
-            <li><router-link to="/bootstrap">Components</router-link></li>
-            <li><router-link to="/form-validation">Form Validation</router-link></li>
+            <router-link tag="li" active-class="active" to="/" exact><a>Home</a></router-link>
+            <router-link tag="li" active-class="active" to="/bootstrap"><a>Components</a></router-link>
+            <router-link tag="li" active-class="active" to="/form-validation"><a>Form Validation</a></router-link>
           </ul>
+        </div>
         </nav>
       </div>
     </div>
